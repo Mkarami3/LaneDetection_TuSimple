5caaa872cfe98d73e80df3ea9dced5ae512e8cdf
Mohammad Karami <42261815+Mkarami3@users.noreply.github.com>
2020-07-02 11:48:36 -0400

    Update AlexNet_LaneDetection.py

--- TuSimple/AlexNet_LaneDetection.py
+++ TuSimple/AlexNet_LaneDetection.py
@@ -1,10 +1,6 @@
 '''
-- Deal with non-valid points, number of points per lane --> Done
-- Normalizing labels dataset or gt labels correspond to image?! --> Done
-- Writing a Video based on a new images --> Done
-- Create Figure for drawing train loss, write a callback function
-- How to resize picture, keep aspect ratio?
-- Crop the image by the height of lane available on json file
+- Apply AlexNet as regression CNN on TuSimple data set
+- Results were not satisfactory!
 '''
 '''
 0313---2858 files
